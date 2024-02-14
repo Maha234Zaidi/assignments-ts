99faab074bd6a62bdd519c0f866bddadba403c2c
Maha_Zaidi <124033522+Maha234Zaidi@users.noreply.github.com>
2024-02-14 23:56:43 +0500

    Add files via upload

---
+++ assignment 05/index.ts
@@ -0,0 +1,18 @@
+// assignment no 05
+let age = 20;
+if(age < 18){
+    console.log("You are a minor");
+}
+else if(age == 18 && age <= 65){
+    console.log("You are an adult");
+}
+else if(age < 65){
+    console.log("You are a senior");
+}
+else{
+    console.log("Invalid age");
+}
+
+
+
+
